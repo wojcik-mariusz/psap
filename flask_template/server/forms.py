c278d1198358f86140f3ae7f1fddb8089bbbbf96
Mariusz <wojcikk.mariusz@gmail.com>
2022-03-20 19:58:27 +0100

    add multiply fields in forms, and update form templates

--- flask_template/server/forms.py
+++ flask_template/server/forms.py
@@ -1,10 +1,17 @@
+'''Flask config'''
+
 from flask_wtf import FlaskForm
-from wtforms.fields import StringField, SelectField, IntegerField
+from wtforms import widgets
+from wtforms.fields import StringField, SelectField, IntegerField, SelectMultipleField, SubmitField
 from wtforms.validators import data_required, length
 
 
+class MultiCheckboxField(SelectMultipleField):
+    widget = widgets.ListWidget(prefix_label=False)
+    option_widget = widgets.CheckboxInput()
+
 class NewEvent(FlaskForm):
-    new_event_voivodeship = SelectField("Select voivodeship", choices=[
+    new_event_voivodeship = SelectField("Select voivodeship:", choices=[
         "dolnośląskie", "kujawsko-pomorskie", "lubelskie", "lubuskie", "łódzkie", "małopolskie", "mazowieckie",
         "opolskie", "podkarpackie", "podlaskie", "pomorskie", "śląskie", "świętokrzyskie", "warmińsko-mazurskie",
         "wielkopolskie", "zachodniopomorskie"
@@ -14,7 +21,8 @@ class NewEvent(FlaskForm):
     new_event_town = StringField("Town:", validators=[data_required()])
     new_event_street = StringField("Street:")
     new_event_street_number = IntegerField("Number:", validators=[data_required()])
-    new_event_type_of_emergency_services_needed = SelectField("Forward event to:", choices=["Straż Pożarna", "Policja", "Ratownictwo medyczne"])
+    new_event_type_of_emergency_services_needed = MultiCheckboxField('Label', choices=["Straż Pożarna", "Policja", "Ratownictwo medyczne"])
     new_event_caller_telephone_number = IntegerField("Telephone number:", validators=[data_required(), length(min=9)])
-    new_event_caller_name = StringField()
-    new_event_caller_surname = StringField()
+    new_event_caller_name = StringField("Caller name:")
+    new_event_caller_surname = StringField("Caller surname:")
+    new_event_confirm_button = SubmitField("Apply event")

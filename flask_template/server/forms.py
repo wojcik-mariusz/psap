63ffaac9c6e251f2f6df9e04629a61e4fd4b1165
MFireIt <wojcikk.mariusz@gmail.com>
2022-03-20 18:59:42 +0100

    add form.py and init form NewEvent , add flask_wtf to requirements, update .gitignore for .idea files, init base.html in templates and add booststrap into it

---
+++ flask_template/server/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, SelectField, IntegerField
+from wtforms.validators import data_required, length
+
+
+class NewEvent(FlaskForm):
+    new_event_voivodeship = SelectField("Select voivodeship", choices=[
+        "dolnośląskie", "kujawsko-pomorskie", "lubelskie", "lubuskie", "łódzkie", "małopolskie", "mazowieckie",
+        "opolskie", "podkarpackie", "podlaskie", "pomorskie", "śląskie", "świętokrzyskie", "warmińsko-mazurskie",
+        "wielkopolskie", "zachodniopomorskie"
+    ])
+    new_event_district = StringField("District:", validators=[data_required()])
+    new_event_community = StringField("Community:" ,validators=[data_required()])
+    new_event_town = StringField("Town:", validators=[data_required()])
+    new_event_street = StringField("Street:")
+    new_event_street_number = IntegerField("Number:", validators=[data_required()])
+    new_event_type_of_emergency_services_needed = SelectField("Forward event to:", choices=["Straż Pożarna", "Policja", "Ratownictwo medyczne"])
+    new_event_caller_telephone_number = IntegerField("Telephone number:", validators=[data_required(), length(min=9)])
+    new_event_caller_name = StringField()
+    new_event_caller_surname = StringField()
--- flask_template/server/routes.py
+++ flask_template/server/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import SERVER_BLUEPRINT
+from flask_template.server.forms import NewEvent
 
 @SERVER_BLUEPRINT.route("/")
 def home():
-    return render_template("index.html")
+    form = NewEvent()
+    return render_template("index.html", form=form)

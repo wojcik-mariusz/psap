e12e15aab594d0c8161d74d9113d8fd84291b717
Mariusz W <wojcikk.mariusz@gmail.com>
2022-04-19 23:13:22 +0200

    clean code -> missing afrerspaces

--- flask_template/db/db_tools.py
+++ flask_template/db/db_tools.py
@@ -1,11 +1,7 @@
-import datetime
-
 from . import DB
 
 from sqlalchemy.orm import relationship
 
-
-# dir should be named 'Models'
 
 class Event(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
@@ -27,6 +23,7 @@ class EventAddress(DB.Model):
     street_number = DB.Column(DB.Integer, nullable=False)
     event_id = DB.Column(DB.Integer, DB.ForeignKey("event.id"))
 
+
 class EventService(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     fw_to_police = DB.Column(DB.Boolean)
@@ -34,10 +31,12 @@ class EventService(DB.Model):
     fw_to_fire_service = DB.Column(DB.Boolean)
     event_id = DB.Column(DB.Integer, DB.ForeignKey("event.id"))
 
+
 class EventReporter(DB.Model):
     id = DB.Column(DB.Integer, primary_key=True)
     caller_name = DB.Column(DB.String)
     caller_telephone_number = DB.Column(DB.Integer, nullable=False)
     caller_surname = DB.Column(DB.String)
     event_id = DB.Column(DB.Integer, DB.ForeignKey("event.id"))
+
 
